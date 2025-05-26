ffe0ff4867f842c3769b3b701d047ab19dbe6e1c
Chafi CASSIM <chafi@cc-consulting.io>
2025-05-26 13:25:45 +0200

    chore: update Blocky version to 0.2.8.1 and enhance logging in config script

--- blocky/rootfs/etc/cont-init.d/config.sh
+++ blocky/rootfs/etc/cont-init.d/config.sh
@@ -11,3 +11,8 @@ tempio \
     -conf /data/options.json \
     -template /usr/share/tempio/blocky.gtpl \
     -out "${CONFIG}"
+
+bashio::log.info "Generated Blocky configuration:"
+bashio::log.info "================================"
+cat "${CONFIG}"
+bashio::log.info "================================"
